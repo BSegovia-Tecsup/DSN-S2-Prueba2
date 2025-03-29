fd857aa69740dd8777017ecf901b178c5bed8ddb
Bryan Segovia <bryan.segovia@tecsup.edu.pe>
2025-03-29 13:01:58 -0500

    Bienvenida

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/bienvenido');
 });
 
 Route::get('/bienvenido', function () {
